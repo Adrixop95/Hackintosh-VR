ca9ac82b8cc27f3f73ac1f100f70ad25ae4f6468
Kamil Karpiński <kamil.k3@outlook.com>
2019-04-01 15:52:23 +0200

    First Release
    Hackintosh VR

---
+++ src/Assets/Scripts/CameraRaycast.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRaycast : MonoBehaviour {
+
+    public  float       raycastMaxDelay = 60f;
+    public  float       raycastDistance = 10f;
+
+    public  LayerMask[] ignoreLayer;
+
+    private GameObject  lastObject;
+    private float       raycastTimer;
+
+    // ####################################################################################################
+    void Start() {
+        
+    }
+
+    // ----------------------------------------------------------------------------------------------------
+    void Update() {
+        RaycastHit();
+    }
+
+    // ####################################################################################################
+    private void RaycastHit() {
+        RaycastHit  hit;
+        Ray         ray     =   new Ray( transform.position, transform.forward );
+
+        if ( Physics.Raycast( ray, out hit, raycastDistance ) ) {
+            GameObject  hitObject   =   hit.collider.gameObject;
+            raycastTimer            +=  Time.deltaTime;
+
+            if ( hitObject != lastObject ) {
+                raycastTimer    =   0f;
+                lastObject      =   hitObject;
+            }
+
+            if ( raycastTimer > raycastMaxDelay ) {
+                raycastTimer = raycastMaxDelay;
+            }
+
+        } else {
+            raycastTimer    =   0f;
+            lastObject      =   null;
+        }
+    }
+
+    // ####################################################################################################
+    public GameObject SelectedObject {
+        get { return lastObject; }
+    }
+
+    // ----------------------------------------------------------------------------------------------------
+    public float SelectedTime {
+        get { return raycastTimer; }
+    }
+
+    // ####################################################################################################
+}
---
+++ src/Assets/Scripts/MenuControl.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class MenuControl : MonoBehaviour {
+
+    public  CameraRaycast   raycast;
+    public  float           raycastTime     =   2.5f;
+
+    public  GameObject      mainMenu;
+    public  GameObject      videoMenu;
+
+    public  VideoPlayer     videoPlane;
+    public  VideoClip[]     videos;
+
+    // ####################################################################################################
+    void Start() {
+        
+    }
+
+    // ----------------------------------------------------------------------------------------------------
+    void Update() {
+        OnDetectUI( raycast );
+    }
+
+    // ####################################################################################################
+    private void OnDetectUI( CameraRaycast raycast ) {
+        if ( raycast.SelectedTime >= raycastTime ) {
+            GameObject  raycastObject   =   raycast.SelectedObject;
+
+            if ( raycastObject == null ) { return; }
+            if ( raycastObject.name == "Button Play" ) {
+                int video   =   Random.Range( 0, videos.Length );
+                ShowMenu( false );
+                ShowVideo( true );
+                PlayVideo( video );
+
+            } else if ( raycastObject.name == "Button Blose" ) {
+                Application.Quit();
+
+            } else if ( raycastObject.name == "Button Back" ) {
+                if ( videoPlane.isPlaying ) { videoPlane.Stop(); }
+                ShowVideo( false );
+                ShowMenu( true );
+
+            }
+        }
+    }
+
+    // ####################################################################################################
+    private void ShowMenu( bool show ) {
+        mainMenu.SetActive( show );
+    }
+
+    // ----------------------------------------------------------------------------------------------------
+    private void ShowVideo( bool show ) {
+        videoMenu.SetActive( show );
+    }
+
+    // ----------------------------------------------------------------------------------------------------
+    private void PlayVideo( int video ) {
+        videoPlane.clip     =   videos[ video ];
+        videoPlane.Play();
+    }
+
+    // ####################################################################################################
+}
